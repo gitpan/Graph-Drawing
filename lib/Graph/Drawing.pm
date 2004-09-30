43893313dd092421669dd9085b655bb49e8ad5d9
Gene Boggs <gene@cpan.org>
2004-09-30 02:03:50 +0000

    Import of GENE/Graph-Drawing-0.0902_1 from CPAN.
    gitpan-cpan-distribution: Graph-Drawing
    gitpan-cpan-version:      0.0902_1
    gitpan-cpan-path:         GENE/Graph-Drawing-0.0902_1.tar.gz
    gitpan-cpan-author:       GENE
    gitpan-cpan-maturity:     developer

--- lib/Graph/Drawing.pm
+++ lib/Graph/Drawing.pm
@@ -1,81 +1,40 @@
-# $Id: Drawing.pm,v 1.2 2003/09/28 08:13:11 gene Exp $
+# $Id: Drawing.pm,v 1.4 2004/09/30 02:00:03 gene Exp $
 
 package Graph::Drawing;
 
-# This is the distribution version:
-use vars qw($VERSION);
-$VERSION = '0.0901';
+our $VERSION = '0.0902_1';
 
-use strict;
-use base qw(Graph::Drawing::Base);
-use Graph::Drawing::Surface;
-use Graph::Drawing::Vertex;
 1;
 
 __END__
 
 =head1 NAME
 
-Graph::Drawing - Graph drawing functionality
+Graph::Drawing - OBSOLETE Graph drawing functionality
 
 =head1 SYNOPSIS
 
-  use Graph::Drawing::ForceDirected;       # Not implemented.
-  use Graph::Drawing::SimulatedAnnealing;  # Not implemented.
-  use Graph::Drawing::Magnetic;            # Not implemented.
-  use Graph::Drawing::Heirarchical;        # Not implemented.
-  use Graph::Drawing::Orthogonal;          # Not implemented.
-
-  use Graph::Drawing::Random;
+  use Graph::Layout::Aesthetic;  # instead
 
 =head1 DESCRIPTION
 
-This module is a base class from which the C<Graph::Drawing> 
-subclasses derive common functionality.
-
-This module is a subclass of C<Graph::Weighted>, which in turn is a
-subclass of C<Graph::Directed>.  Thus, every appropriate method 
-available in those modules, is also available to a C<Graph::Drawing>
-object.
-
-Please see the <Graph::Drawing::Base> module for basic method 
-descriptions, and the individual C<Graph::Drawing> subclass 
-C<SYNOPSIS> sections for usage descriptions.
-
-Also, see the distribution eg/ directory for working examples.
+This module is now obsolete but used to be a base class for other
+C<Graph::Drawing> subclasses.  Then C<Graph::Layout::Aesthetic>
+silently appeared ..despite my request for development and better
+ideas below.  Anyway...  I am happy that there is finally a decent
+way to draw graphs on the CPAN now.
 
 =head1 SEE ALSO
 
-L<Graph::Base>
-
-L<Graph::Weighted>
-
-L<Graph::Drawing::Base>
-
-L<Graph::Drawing::Surface>
-
-L<Graph::Drawing::Vertex>
-
-L<Graph::Drawing::Random>
+L<Graph::Layout::Aesthetic>
 
 =head1 TO DO
 
-A little less than everything.
+Play with C<Graph::Layout::Aesthetic>.
 
+B<OLD COMMENT:>
 If you would like to contribute to this project, please contact me
 and I will rejoice.
-
-=head1 DISCLAIMER
-
-This entire distribution is currently under heavy development.
-
-Nearly every method, argument and bit of documentation currently have 
-(possible annoying) restrictions, caveats and deficiencies, that will 
-change as development progresses.
-
-* Ack!  So I was accidentally uploading version typos.  For example, 
-instead of the correct, '0.04', I did '0.4' for versions 0.02, 0.03 
-and 0.04.  When you see this in my archives, do not be fooled.
 
 =head1 AUTHOR
 
--- t/drawing.t
+++ t/drawing.t
@@ -1,11 +1,5 @@
-use Test::More 'no_plan';#tests => 1;
-
-BEGIN { use_ok 'Graph::Drawing' };
-
-my $g;
-
-eval {
-    $g = Graph::Drawing->new();
-};
-isa_ok $g, 'Graph::Drawing';
-ok !$@, 'created with no arguments';
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Test::More tests => 1;
+use_ok 'Graph::Drawing';
